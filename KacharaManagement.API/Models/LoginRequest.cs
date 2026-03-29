9996f170c6032b7531024ed474cf0bb35d920b1f
Tejaswini3107 <tejaswini.medam@epitech.eu>
2026-03-29 17:21:40 +0200

    fix: add required modifier to LoginRequest properties

--- KacharaManagement.API/Models/LoginRequest.cs
+++ KacharaManagement.API/Models/LoginRequest.cs
@@ -2,7 +2,7 @@ namespace KacharaManagement.API.Models
 {
     public class LoginRequest
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
